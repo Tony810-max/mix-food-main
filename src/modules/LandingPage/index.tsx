746463ab4377c71b8d83d72ec4574da0620c211a
Tony810-mã <quangvu37781@gmail.com>
2025-02-24 12:09:44 +0700

    feat: banner landing page

--- src/modules/LandingPage/index.tsx
+++ src/modules/LandingPage/index.tsx
@@ -1,11 +1,12 @@
-import { useTranslations } from 'next-intl';
 import React from 'react';
+import Banner from './components/Banner';
 
 const LandingPage = () => {
-  const t = useTranslations('HomePage');
   return (
     <div className='mt-20 flex justify-center lg:mt-[6.5rem] xl:mt-20'>
-      <div className='container'>landing page</div>
+      <div className='container'>
+        <Banner />
+      </div>
     </div>
   );
 };
