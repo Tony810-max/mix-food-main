28991fcad050c1701ecd88b453d0355a99f44345
Tony810-mã <quangvu37781@gmail.com>
2025-03-01 22:05:47 +0700

    feat: feature section

---
+++ src/modules/LandingPage/components/Feature.tsx
@@ -0,0 +1,18 @@
+import CardFood from '@/components/ui/CardFood';
+import { useTranslations } from 'next-intl';
+import React from 'react';
+import { getDataFeature } from '../utils/const';
+
+const Feature = () => {
+  const t = useTranslations('HomePage');
+  const dataFeature = getDataFeature(t);
+  return (
+    <div className='grid grid-cols-2 gap-3 p-4 sm:grid-cols-3 lg:grid-cols-5'>
+      {dataFeature?.map((feature) => (
+        <CardFood key={feature?.id} img={feature?.img} title={feature?.title} desr={feature?.desr} />
+      ))}
+    </div>
+  );
+};
+
+export default Feature;
--- src/modules/LandingPage/index.tsx
+++ src/modules/LandingPage/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './components/Banner';
+import Feature from './components/Feature';
 
 const LandingPage = () => {
   return (
     <div className='mt-20 flex justify-center lg:mt-[6.5rem] xl:mt-20'>
       <div className='container'>
         <Banner />
+        <Feature />
       </div>
     </div>
   );
