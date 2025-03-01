244e8c36671e2dca9c9d2d276c05482b29a9f78c
Tony810-mã <quangvu37781@gmail.com>
2025-03-01 22:37:02 +0700

    fix: padding  banner

--- src/modules/LandingPage/components/Banner/index.tsx
+++ src/modules/LandingPage/components/Banner/index.tsx
@@ -5,8 +5,10 @@ import SearchFood from './SearchFood';
 
 const Banner = () => {
   return (
-    <div className='relative aspect-[2/1]'>
-      <Image src={'/images/landingPage/banner.webp'} alt='img banner' fill priority unoptimized className='z-0' />
+    <div className='relative py-4'>
+      <div className='relative aspect-[2/1]'>
+        <Image src={'/images/landingPage/banner.webp'} alt='img banner' fill priority unoptimized className='z-0' />
+      </div>
       <div className='absolute flex flex-col items-center justify-center gap-8 text-white max-sm:h-full max-sm:w-full sm:right-10 sm:bottom-10 sm:items-end sm:justify-normal'>
         <Heading />
         <SearchFood />
--- src/modules/LandingPage/components/Feature.tsx
+++ src/modules/LandingPage/components/Feature.tsx
@@ -7,7 +7,7 @@ const Feature = () => {
   const t = useTranslations('HomePage');
   const dataFeature = getDataFeature(t);
   return (
-    <div className='grid grid-cols-2 gap-3 p-4 sm:grid-cols-3 lg:grid-cols-5'>
+    <div className='grid grid-cols-2 gap-3 py-4 max-lg:px-4 sm:grid-cols-3 lg:grid-cols-5'>
       {dataFeature?.map((feature) => (
         <CardFood key={feature?.id} img={feature?.img} title={feature?.title} desr={feature?.desr} />
       ))}
