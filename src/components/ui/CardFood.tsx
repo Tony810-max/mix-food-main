69742abf09fb2cae2a9667ee36d7c594f818932f
Tony810-mã <quangvu37781@gmail.com>
2025-03-02 10:39:48 +0700

    feat: popular

--- src/components/ui/CardFood.tsx
+++ src/components/ui/CardFood.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { Tooltip } from '@heroui/react';
+import clsx from 'clsx';
 import Image from 'next/image';
 import React from 'react';
 
@@ -5,17 +8,20 @@ interface ICard {
   img: string;
   title: string;
   desr: string;
+  className?: string;
 }
 
-const CardFood: React.FC<ICard> = ({ desr, img, title }) => {
+const CardFood: React.FC<ICard> = ({ desr, img, title, className }) => {
   return (
-    <div className='space-y-3 rounded-md border border-primary p-4'>
+    <div className={clsx('space-y-3 rounded-md', className)}>
       <div className='relative aspect-[3/2]'>
         <Image src={img} alt='img Feature' className='rounded-lg' fill unoptimized priority />
       </div>
       <div className='flex flex-col gap-1'>
         <span className='font-bold text-base'>{title}</span>
-        <span className='line-clamp-3 text-primary text-sm'>{desr}</span>
+        <Tooltip content={<span className='inline-block max-w-52'>{desr}</span>}>
+          <span className='line-clamp-2 text-primary text-sm lg:text-justify'>{desr}</span>
+        </Tooltip>
       </div>
     </div>
   );
---
+++ src/modules/LandingPage/components/Popular.tsx
@@ -0,0 +1,22 @@
+import CardFood from '@/components/ui/CardFood';
+import { useTranslations } from 'next-intl';
+import React from 'react';
+import { getDataPopular } from '../utils/const';
+import Heading from './Heading';
+
+const Popular = () => {
+  const t = useTranslations('HomePage');
+  const populars = getDataPopular(t);
+  return (
+    <div className='py-4 max-xl:px-4'>
+      <Heading title="What's Popular" />
+      <div className='grid grid-cols-2 gap-3 py-4 lg:grid-cols-4'>
+        {populars?.map((popular) => (
+          <CardFood key={popular?.id} img={popular?.img} title={popular?.title} desr={popular?.desr} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Popular;
