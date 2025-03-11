66fdc0f99852e3453a057943a2e323a66815e44e
Tony810-mã <quangvu37781@gmail.com>
2025-03-11 17:44:26 +0700

    fix: width height banner

--- src/modules/LandingPage/components/Banner/index.tsx
+++ src/modules/LandingPage/components/Banner/index.tsx
@@ -5,8 +5,8 @@ import SearchFood from './SearchFood';
 
 const Banner = () => {
   return (
-    <div className='relative '>
-      <div className='relative min-h-[calc(100vh-80px)] w-full'>
+    <div className='relative'>
+      <div className='relative aspect-[3/2] lg:aspect-auto lg:min-h-[calc(100vh-71.97px)]'>
         <Image src={'/images/landingPage/banner.webp'} alt='img banner' fill priority unoptimized />
       </div>
       <div className=' absolute flex flex-col items-center justify-center gap-8 text-white max-sm:top-0 max-sm:h-full max-sm:w-full sm:right-10 sm:bottom-10 sm:items-end sm:justify-normal'>
