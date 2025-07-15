1595e8215a05348d14fceacb9eccb52d1893b7c4
Tony810-mã <quangvu37781@gmail.com>
2025-07-15 22:52:03 +0700

    fix: fix error

--- src/api/changePassword/mutations.ts
+++ src/api/changePassword/mutations.ts
@@ -1,9 +1,9 @@
-import type { UseMutationOptions } from '@tanstack/react-query';
-import { useMutation } from '@tanstack/react-query';
-import type { AxiosError } from 'axios';
+// import type { UseMutationOptions } from '@tanstack/react-query';
+// import { useMutation } from '@tanstack/react-query';
+// import type { AxiosError } from 'axios';
 
-export const useForgotPasswordMutation = (
-  options?: UseMutationOptions<ForgotPasswordResponse, AxiosError, IForgotPasswordBody>
-) => {
-  return useMutation({ mutationFn: forgotPasswordRequest, ...options });
-};
+// export const useForgotPasswordMutation = (
+//   options?: UseMutationOptions<ForgotPasswordResponse, AxiosError, IForgotPasswordBody>
+// ) => {
+//   return useMutation({ mutationFn: forgotPasswordRequest, ...options });
+// };
