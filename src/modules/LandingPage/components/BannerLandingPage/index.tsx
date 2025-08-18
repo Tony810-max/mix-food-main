c9eb4a7760326c7991ac3affc449b645e95628d5
Tony810-mã <quangvu37781@gmail.com>
2025-08-18 23:07:56 +0700

    feat: change banner

--- src/modules/LandingPage/components/BannerLandingPage/index.tsx
+++ src/modules/LandingPage/components/BannerLandingPage/index.tsx
@@ -6,16 +6,17 @@ import SearchFood from './SearchFood';
 const BannerLandingPage = () => {
   return (
     <div className='relative h-[calc(100vh-60px)] text-white'>
+      <div className='absolute inset-0 z-20 bg-black/40'></div>
       <Image
-        src='/images/landingPage/banner.webp'
+        src='/images/landingPage/banner.jpg'
         alt='banner'
         fill
         priority
         unoptimized
-        className='z-0 object-cover'
+        className='z-10 object-cover'
       />
 
-      <div className='container relative z-10 mx-auto flex h-full flex-col items-center justify-center md:items-end md:justify-end'>
+      <div className='container relative z-30 mx-auto flex h-full flex-col items-center justify-center md:items-end md:justify-end'>
         <div className='space-y-8 pb-11'>
           <HeadingBanner />
           <SearchFood />
