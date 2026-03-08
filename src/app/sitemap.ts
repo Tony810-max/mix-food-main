62ce8af3e39d8b3aa80c10c1f61cf56f39b91c74
tony810-max <quangvu37781@gmail.com>
2026-03-08 12:38:47 +0700

    feat: seo website

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,9 @@ import type { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   try {
-    const privateRoutes = Object.values(ROUTES).filter((route) => route !== ROUTES.PROFILE);
+    const privateRoutes = Object.values(ROUTES).filter(
+      (route) => route !== ROUTES.PROFILE && route !== ROUTES.FACEBOOK
+    );
     const staticPages: MetadataRoute.Sitemap = privateRoutes.map((route) => ({
       url: `${siteConfig.appUrl}${route}`,
       lastModified: new Date(),
