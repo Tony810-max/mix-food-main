41c946f263b744ae24b91d2cf8a67650ab8713b7
tony810-max <quangvu37781@gmail.com>
2026-03-08 12:58:45 +0700

    feat: seo website

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,10 +7,5 @@ export const siteConfig = {
   name: 'Mix Food - The best food in the Da Nang area',
   metaTitle: 'Mix Food - The best food in the Da Nang area',
   description: 'Mix Food - The best food in the Da Nang area',
-  ogImage: {
-    url: '/images/landingPage/banner.webp',
-    width: 1200,
-    height: 630,
-    alt: 'Mix Food - The best food in the Da Nang area',
-  },
+  ogImage: '/images/landingPage/banner.webp',
 };
