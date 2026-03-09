f3d2e8029f8170d8cdf258006090f6e8fb144a36
tony810-max <quangvu37781@gmail.com>
2026-03-09 11:52:59 +0700

    feat: seo webiste

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -1,9 +1,3 @@
 import ProfilePage from '@/modules/ProfilePage';
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Profile',
-  description: 'Profile Restaurant Thailand Food',
-};
 
 export default ProfilePage;
